9a17d50ff7df525e1c6f83c53b64ea9aec916874
Oleksandr Lavrushko <olavrushko@sygic.com>
2022-07-20 17:28:52 +0200

    Add SygicMaps Swift Package as URL to binary XCFramework with dependency on SygicAuth

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SygicMaps",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "SygicMaps",
+            targets: ["SygicMaps"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/olavrushkosygic/SygicAuthSPM.git", .upToNextMajor(from: "1.3.1"))
+    ],
+    targets: [
+        .binaryTarget(
+            name: "SygicMaps",
+            url: "https://repository.sygic.com/repository/maven-public/com/sygic/sdk/maps-ios/21.0.3/maps-ios-21.0.3.zip",
+            checksum: "563810da6346230ac984894709fa4dc2d0a61cdcb100f6e511d6070121a5e258")
+    ]
+)
